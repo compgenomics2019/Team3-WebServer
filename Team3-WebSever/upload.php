2d848e0f7b688c11dda4b88a5c828054d7cc6d2d
George Gruenhagen <ggruenhagen3@gatech.edu>
2019-03-13 14:39:24 -0400

    Debugging

--- Team3-WebSever/upload.php
+++ Team3-WebSever/upload.php
@@ -8,7 +8,7 @@ $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-		echo $target_file
+		// echo $target_file
 		// echo basename( $_FILES["fileToUpload"]["name"])
         echo "Sorry, there was an error uploading your file.";
     }
