3cdc721ed9f5a71dc3bb40c8bd254a3942b7d9ac
George Gruenhagen <ggruenhagen3@gatech.edu>
2019-03-14 11:42:55 -0400

    Debugging

--- Team3-WebSever/upload.php
+++ Team3-WebSever/upload.php
@@ -9,7 +9,7 @@ if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
 		// echo $target_file
-		echo basename( $_FILES["fileToUpload"]["name"])
+		// echo basename( $_FILES["fileToUpload"]["name"])
         echo "Sorry, there was an error uploading your file.";
     }
 ?>
