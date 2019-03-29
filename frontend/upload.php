af816d9a33d2f12bae6efface1c629232b97c88a
Jialin Ma <marlin-@gmx.cn>
2019-03-29 19:28:35 -0400

    Update upload.php

--- frontend/upload.php
+++ frontend/upload.php
@@ -11,5 +11,39 @@ echo json_encode($Myfile);
 <h2>The file content</h2>
 <?php
 $fileContent = file_get_contents($Myfile['tmp_name']);
-echo $fileContent
+echo "<pre>$fileContent</pre>";
+?>
+
+<h2>Try running commands on the file</h2>
+<h4>whoami</h4>
+<?php
+echo exec("whoami");
+?>
+
+<h4>mkdir results</h4>
+<?php
+$ok = mkdir("results");
+if (!$ok)
+    echo "<pre>mkdir failed.....</pre>";
+?>
+
+<h4>pwd</h4>
+<?php
+echo exec("pwd");
+?>
+
+<h4>mv the file</h4>
+<?php
+echo "mv " . $Myfile['tmp_name'] . " " . $_FILES["fileToUpload"]["name"];
+$ok = exec("mv " . $Myfile['tmp_name'] . " " . $_FILES["fileToUpload"]["name"]);
+if (!$ok)
+    echo "<pre>mv failed</pre>"
+?>
+
+<h4>ls</h4>
+<?php
+$fname = $Myfile['tmp_name'];
+//$res = shell_exec("ls -alh $fname");
+$res = shell_exec("ls -alh .");
+echo "<pre>$res</pre>";
 ?>
