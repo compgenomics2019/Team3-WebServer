fbd0413ade369eaac0b06f007991838880d12e75
Jialin Ma <marlin-@gmx.cn>
2019-04-02 00:52:35 -0400

    Simplifying serve.sh

--- serve.sh
+++ serve.sh
@@ -3,35 +3,13 @@
 set -e
 trap 'kill $(jobs -p)' EXIT
 
-echo
-echo Start Frontend Server
-echo ---------------------------------
-echo
-
-cd frontend
-python3 -m http.server 4444 > /dev/null &
-cd ..
-python3 -mwebbrowser http://localhost:4444
-
-
-echo
-echo Activate backend venv environment
-echo ---------------------------------
-echo
-
-. ./backend/venv/bin/activate
-
-
-echo
-echo Start Backend Server
-echo ---------------------------------
-echo
+#. ./backend/venv/bin/activate
+VENV_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/backend/venv
+FLASK_BIN=$VENV_PATH/bin/flask
 
 cd backend
-FLASK_APP=server.py FLASK_ENV=development flask run &
 
-echo
-echo ---------------------------------
-echo
+FLASK_APP=server.py FLASK_RUN_PORT=9981 FLASK_ENV=development \
+  $FLASK_BIN run --host=0.0.0.0 &
 
 wait
