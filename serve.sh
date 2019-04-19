b9b9a6a701512968a9be0c58216cd4255da25c02
Jialin Ma <marlin-@gmx.cn>
2019-04-19 13:38:15 -0400

    Modify serve.sh

--- serve.sh
+++ serve.sh
@@ -4,12 +4,12 @@ set -e
 trap 'kill $(jobs -p)' EXIT
 
 #. ./backend/venv/bin/activate
-VENV_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/backend/venv
-FLASK_BIN=$VENV_PATH/bin/flask
+#VENV_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/backend/venv
+#FLASK_BIN=$VENV_PATH/bin/flask
 
 cd backend
 
 FLASK_APP=server.py FLASK_RUN_PORT=9981 \
-  $FLASK_BIN run --host=0.0.0.0 &
+  flask run --host=0.0.0.0 &
 
 wait
