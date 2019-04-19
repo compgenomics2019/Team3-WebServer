c74b5a3b70eb266589e8882ea203b80cf86f8e6c
Sharma, Siddhartha <ssharma435@gatech.edu>
2019-04-19 14:53:42 -0400

    Update FANI.sh

--- backend/scripts/FANI.sh
+++ backend/scripts/FANI.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# Script written to find out 20 of the most diverse sequences from the 173 Listeria Sequences extracted from NCBI
+# Script not involved in the final pipeline.
+# Script was run just once to extract the 20 most diverse sequences.
+
 readlink -e /projects/VirtualHost/predictc/Listeria_Sequences/AllGenomes/* > QueryList.txt
 
 mash sketch -o Trial -l QueryList.txt
@@ -21,21 +26,26 @@ sort -k2 -n -t ',' AverageIdentity.txt > SortedIdentity.txt
 head -20 SortedIdentity.txt | cut -d',' -f1 | cut -d'/' -f7 > LeastDistance.txt
 tail -20 SortedIdentity.txt | cut -d',' -f1 | cut -d'/' -f7 > MostDistance.txt
 
-cd /projects/VirtualHost/predictc/Listeria_Sequences/AllGenomes
+#cd /projects/VirtualHost/predictc/Listeria_Sequences/AllGenomes
 
-mkdir ../MostDistance
-mkdir ../LeastDistance
+mkdir /projects/VirtualHost/predictc/Listeria_Sequences/MostDistance
+mkdir /projects/VirtualHost/predictc/Listeria_Sequences/LeastDistance
 
-cp $(cat /projects/VirtualHost/predictc/scripts/MostDistance.txt) ../MostDistance
-cp $(cat /projects/VirtualHost/predictc/scripts/LeastDistance.txt) ../LeastDistance
+#Transfer files listed in the two text files from the directory containing all the genomes to the required Directory.
 
-mv /projects/VirtualHost/predictc/scripts/QueryList.txt ../
-mv /projects/VirtualHost/predictc/scripts/FinalOutput.txt ../
-mv /projects/VirtualHost/predictc/scripts/Trial.msh ../
-mv /projects/VirtualHost/predictc/scripts/AverageIdentity.txt ../
-mv /projects/VirtualHost/predictc/scripts/SortedIdentity.txt ../
-mv /projects/VirtualHost/predictc/scripts/LeastDistance.txt ../
-mv /projects/VirtualHost/predictc/scripts/MostDistance.txt ../
+rsync --files-from=MostDistance.txt /projects/VirtualHost/predictc/Listeria_Sequences/AllGenomes/ /projects/VirtualHost/predictc/Listeria_Sequences/MostDistance/
+rsync --files-from=LeastDistance.txt /projects/VirtualHost/predictc/Listeria_Sequences/AllGenomes/ /projects/VirtualHost/predictc/Listeria_Sequences/LeastDistance/
+
+#cp $(cat /projects/VirtualHost/predictc/scripts/MostDistance.txt) ../MostDistance
+#cp $(cat /projects/VirtualHost/predictc/scripts/LeastDistance.txt) ../LeastDistance
+
+mv QueryList.txt /projects/VirtualHost/predictc/Listeria_Sequences
+mv FinalOutput.txt /projects/VirtualHost/predictc/Listeria_Sequences
+mv Trial.msh /projects/VirtualHost/predictc/Listeria_Sequences
+mv AverageIdentity.txt /projects/VirtualHost/predictc/Listeria_Sequences
+mv SortedIdentity.txt /projects/VirtualHost/predictc/Listeria_Sequences
+mv LeastDistance.txt /projects/VirtualHost/predictc/Listeria_Sequences
+mv MostDistance.txt /projects/VirtualHost/predictc/Listeria_Sequences
 
 #rm QueryList.txt
 #rm FinalOutput.txt
