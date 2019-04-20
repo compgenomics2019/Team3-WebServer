efd98838c5666f92eebe2d894fcd17077e5190f9
Sharma, Siddhartha <ssharma435@gatech.edu>
2019-04-20 18:02:18 -0400

    Update test.sh

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -9,6 +9,11 @@ mash sketch -o TrialSketch -l Trial.txt
 mash dist TrialSketch.msh TrialSketch.msh > Out.txt
 sort -k1,1 -k2,2 Out.txt > Sorted.txt
 
+awk '$1 ~ /UserSeq$/{print $2, $3;}' Sorted.txt > DistanceValuesForUserSequence.txt
+sort -k2n DistanceValuesForUserSequence.txt | head -2 | tail -1 | cut -d' ' -f2 > Value.txt
+sort -k2n DistanceValuesForUserSequence.txt | head -2 | tail -1 | cut -d' ' -f1 | cut -d'/' -f7 > Name.txt
+paste Name.txt Value.txt> LeastDistance.txt
+
 python ./scripts/DM.py Sorted.txt
 #python /projects/home/ssharma435/Team3-WebServer/backend/scripts/DM.py Sorted.txt
 
